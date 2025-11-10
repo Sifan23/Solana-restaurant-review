dc6e73b4b2f4ae5b4169712ef6c4b0a69b53be6a
Sifan23 <sifanyg123@gmail.com>
2025-11-10 11:27:32 +0300

    feat: usewallet hook

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,57 +10,56 @@ import ReviewForm from "@/components/Form";
 //Replace with your own Program_id
 const REVIEW_PROGRAM_ID = "GQr3jxU9exnAt421W6RsjBeVNqFVUQZtGpMDRR9Uqfqk";
 
-export default function Home() {   
-        const connection = new web3.Connection(web3.clusterApiUrl("devnet"));
-    const [txid, setTxid] = useState("");
-    const [reviews, setReviews] = useState<Review[]>([]);
+export default function Home() {
+  const connection = new web3.Connection(web3.clusterApiUrl("devnet"));
+  const { publicKey, sendTransaction } = useWallet();
+  const [txid, setTxid] = useState("");
+  const [reviews, setReviews] = useState<Review[]>([]);
 
-    const [title, setTitle] = useState("");
-    const [rating, setRating] = useState(0);
-    const [description, setDescription] = useState("");
+  const [title, setTitle] = useState("");
+  const [rating, setRating] = useState(0);
+  const [description, setDescription] = useState("");
 
-    useEffect(() => {
-        const fetchAccounts = async () => {};
-        fetchAccounts();
-    }, []);
+  useEffect(() => {
+    const fetchAccounts = async () => {};
+    fetchAccounts();
+  }, []);
 
-    const handleSubmit = () => {
-        const review = new Review(title, rating, description);
-        handleTransactionSubmit(review);
-    };
+  const handleSubmit = () => {
+    const review = new Review(title, rating, description);
+    handleTransactionSubmit(review);
+  };
 
-    const handleTransactionSubmit = async (review: Review) => {};
+  const handleTransactionSubmit = async (review: Review) => {};
 
-    return (
-        <main
-            className={`flex min-h-screen flex-col items-center justify-between p-24 `}
-        >
-            <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-                <AppBar />
-            </div>
+  return (
+    <main
+      className={`flex min-h-screen flex-col items-center justify-between p-24 `}
+    >
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <AppBar />
+      </div>
 
-            <div className="after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-                <ReviewForm
-                    title={title}
-                    description={description}
-                    rating={rating}
-                    setTitle={setTitle}
-                    setDescription={setDescription}
-                    setRating={setRating}
-                    handleSubmit={handleSubmit}
-                />
-            </div>
+      <div className="after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
+        <ReviewForm
+          title={title}
+          description={description}
+          rating={rating}
+          setTitle={setTitle}
+          setDescription={setDescription}
+          setRating={setRating}
+          handleSubmit={handleSubmit}
+        />
+      </div>
 
-            {txid && <div>{txid}</div>}
+      {txid && <div>{txid}</div>}
 
-            <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-3 lg:text-left">
-                {reviews &&
-                    reviews.map((review) => {
-                        return (
-                            <ReviewCard key={review.title} review={review} />
-                        );
-                    })}
-            </div>
-        </main>
-    );
+      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-3 lg:text-left">
+        {reviews &&
+          reviews.map((review) => {
+            return <ReviewCard key={review.title} review={review} />;
+          })}
+      </div>
+    </main>
+  );
 }
